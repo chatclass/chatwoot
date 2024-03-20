7a831c35862a047d7238e63cf6e6095390b4c21b
Filipe Felice <filipefelice@gmail.com>
2024-03-20 18:42:25 -0300

    fix: add channel logs

--- app/jobs/webhooks/whatsapp_events_job.rb
+++ app/jobs/webhooks/whatsapp_events_job.rb
@@ -46,10 +46,12 @@ class Webhooks::WhatsappEventsJob < ApplicationJob
   end
 
   def get_channel_from_wb_payload(wb_params)
-    Rails.logger.info "GET CHANNEL"
+    Rails.logger.info "GET CHANNEL #{channel.provider_config['phone_number_id']}"
     phone_number = "+#{wb_params[:entry].first[:changes].first.dig(:value, :metadata, :display_phone_number)}"
     phone_number_id = wb_params[:entry].first[:changes].first.dig(:value, :metadata, :phone_number_id)
     channel = Channel::Whatsapp.find_by(phone_number: phone_number)
+
+    Rails.logger.info "CHANNEL #{channel}"
     # validate to ensure the phone number id matches the whatsapp channel
     return channel if channel && channel.provider_config['phone_number_id'] == phone_number_id
   end
