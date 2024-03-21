111e4a13a6e67f12c796d8e67a9193aac29c8669
Filipe Felice <filipefelice@gmail.com>
2024-03-21 00:18:35 -0300

    fix provider

--- app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
+++ app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
@@ -38,13 +38,7 @@ class Whatsapp::Providers::Whatsapp360DialogCloudApiService < Whatsapp::Provider
         url: "#{ENV.fetch('FRONTEND_URL', nil)}/webhooks/whatsapp/#{whatsapp_channel.phone_number}"
       }.to_json
     )
-
-    if response.success?
-      response.success?
-    else
-      Rails.logger.error response.body
-      nil
-    end
+    response.success?
   end
 
   def api_headers
