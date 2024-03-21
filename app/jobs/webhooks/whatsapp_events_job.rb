06f266fe899e82955aa4772bd76be0a6860d137f
Filipe Felice <filipefelice@gmail.com>
2024-03-21 17:35:17 -0300

    fix: whatsapp cloud incoming service

--- app/jobs/webhooks/whatsapp_events_job.rb
+++ app/jobs/webhooks/whatsapp_events_job.rb
@@ -10,12 +10,13 @@ class Webhooks::WhatsappEventsJob < ApplicationJob
 
     case channel.provider
     when 'whatsapp_cloud'
+      Rails.logger.info "Perform Whatsapp Cloud PARAMS 3"
       Whatsapp::IncomingMessageWhatsappCloudService.new(inbox: channel.inbox, params: params).perform
     #when '360dialogCloudAPI'
     #  Whatsapp::IncomingMessageService.new(inbox: channel.inbox, params: params).perform
-    else
-      Rails.logger.info "Perform PARAMS 3"
-      Whatsapp::IncomingMessageService.new(inbox: channel.inbox, params: params).perform
+    #else
+    #  Rails.logger.info "Perform D360 PARAMS 3"
+    #  Whatsapp::IncomingMessageService.new(inbox: channel.inbox, params: params).perform
     end
   end
 
