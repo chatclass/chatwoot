27ed1d81c071fc13146ab0850be7d6536b76384b
Filipe Felice <filipefelice@gmail.com>
2024-03-21 00:04:56 -0300

    fix to true

--- app/services/whatsapp/providers/whatsapp_360_dialog_service.rb
+++ app/services/whatsapp/providers/whatsapp_360_dialog_service.rb
@@ -38,7 +38,7 @@ class Whatsapp::Providers::Whatsapp360DialogService < Whatsapp::Providers::BaseS
         url: "#{ENV.fetch('FRONTEND_URL', nil)}/webhooks/whatsapp/#{whatsapp_channel.phone_number}"
       }.to_json
     )
-    false
+    true
   end
 
   def api_headers
