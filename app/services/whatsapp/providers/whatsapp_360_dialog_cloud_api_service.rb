27ab3bff01747eb4a5ebb47a138141118ce64acb
Filipe Felice <filipefelice@gmail.com>
2024-03-25 13:45:43 -0300

    fix: class

--- app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
+++ app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
@@ -152,7 +152,7 @@ class Whatsapp::Providers::Whatsapp360DialogCloudApiService < Whatsapp::Provider
     json_hash = JSON.parse(message.content.to_json)
 
     Rails.logger.info "send_interactive_custom_message #{json_hash.class}"
-    Rails.logger.info "send_interactive_custom_message interactive #{message.content["interactive"]}"
+    Rails.logger.info "send_interactive_custom_message interactive #{message.content.class}"
 
     response = HTTParty.post(
       "#{api_base_path}/messages",
