5fb40359a43c1a3056b5f3221bd3efba8db5165f
Filipe Felice <filipefelice@gmail.com>
2024-03-25 17:14:57 -0300

    log parse

--- app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
+++ app/services/whatsapp/providers/whatsapp_360_dialog_cloud_api_service.rb
@@ -158,7 +158,7 @@ class Whatsapp::Providers::Whatsapp360DialogCloudApiService < Whatsapp::Provider
 
     #json_parse = JSON.parse(json_hash.to_json)
 
-    Rails.logger.info "send_interactive_custom_message Parse #{message.content_attributes['type']}"    
+    Rails.logger.info "send_interactive_custom_message Parse #{json_interactive}"    
 
     response = HTTParty.post(
       "#{api_base_path}/messages",
