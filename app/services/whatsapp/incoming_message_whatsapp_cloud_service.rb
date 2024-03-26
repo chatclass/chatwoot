3568b9b316df9b384d97ab62e0312ea5430027ee
Filipe Felice <filipefelice@gmail.com>
2024-03-26 16:57:22 -0300

    fix: remove logs

--- app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
+++ app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
@@ -8,13 +8,10 @@ class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageB
     @processed_params ||= params[:entry].try(:first).try(:[], 'changes').try(:first).try(:[], 'value')
   end
 
-  def download_attachment_file(attachment_payload)
-    Rails.logger.info "Download Attachment #{inbox.channel.media_url(attachment_payload[:id])}"
-    Rails.logger.info "Download Attachment ID #{attachment_payload[:id]}"
-
-    url_response = HTTParty.get(inbox.channel.media_url(attachment_payload[:id]), headers: inbox.channel.api_headers)
+  #def download_attachment_file(attachment_payload)
+  #  url_response = HTTParty.get(inbox.channel.media_url(attachment_payload[:id]), headers: inbox.channel.api_headers)
     # This url response will be failure if the access token has expired.
-    inbox.channel.authorization_error! if url_response.unauthorized?
-    Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers) if url_response.success?
-  end
+  #  inbox.channel.authorization_error! if url_response.unauthorized?
+  #  Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers) if url_response.success?
+  #end
 end
