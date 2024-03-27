b48264b40228acdd2da4625a114410b396bfc014
Filipe Felice <filipefelice@gmail.com>
2024-03-27 00:04:23 -0300

    fix: media url

--- app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
+++ app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
@@ -12,6 +12,9 @@ class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageB
     url_response = HTTParty.get(inbox.channel.media_url(attachment_payload[:id]), headers: inbox.channel.api_headers)
     # This url response will be failure if the access token has expired.
     inbox.channel.authorization_error! if url_response.unauthorized?
-    Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers) if url_response.success?
+
+    d360_media_url = url_response.parsed_response['url'].gsub("lookaside.fbsbx.com", "waba-v2.360dialog.io")
+
+    Down.download(d360_media_url, headers: inbox.channel.api_headers) if url_response.success?
   end
 end
