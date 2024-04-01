ef74f23b851761cff05e60bb9fbc93fecc00ea91
Filipe Felice <filipefelice@gmail.com>
2024-04-01 17:04:53 -0300

    fix message

--- app/models/message.rb
+++ app/models/message.rb
@@ -173,7 +173,7 @@ class Message < ApplicationRecord
   def webhook_data
     data = {
       account: account.webhook_data,
-      raw_payload: additional_attributes,
+      raw_payload: message,
       content_attributes: content_attributes,
       content_type: content_type,
       content: content,
