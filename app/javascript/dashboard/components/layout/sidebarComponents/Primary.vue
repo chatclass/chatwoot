f32a8f0958f2d2aaba2f5eb334d2b064f3bd1533
Filipe Felice <filipefelice@gmail.com>
2024-04-29 12:53:11 -0300

    add: externalUrl

--- app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
@@ -15,6 +15,7 @@
         :icon="menuItem.icon"
         :name="menuItem.label"
         :to="menuItem.toState"
+        :open-in-new-page="menuItem.externalUrl"
         :is-child-menu-active="menuItem.key === activeMenuItem"
       />
     </div>
