bf9f0c5b2891c74ac76189155041fd2647a962a5
Filipe Felice <filipefelice@gmail.com>
2024-05-14 13:28:41 +0000

    fix: teste

--- app/javascript/v3/views/auth/signup/Index.vue
+++ app/javascript/v3/views/auth/signup/Index.vue
@@ -27,7 +27,7 @@
           <div class="text-sm text-slate-800 dark:text-woot-50 px-1">
             <span>{{ $t('REGISTER.HAVE_AN_ACCOUNT') }}</span>
             <router-link class="text-link" to="/app/login">
-              {{
+              TESTE - {{
                 useInstallationName(
                   $t('LOGIN.TITLE'),
                   globalConfig.installationName
